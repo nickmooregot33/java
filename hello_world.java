b21cca7a0ca477fa984465faf8e1235502e713d6
Nick Moore <nickmooregot33@users.noreply.github.com>
2015-12-18 15:20:35 -0600

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,12 @@
+//classic hello world
+
+//also provides a template for the java version of int main{ return 0;}
+
+public class Hello_world {
+  //main method begins execution of java application
+  public static void main(String args[]){
+    System.out.print("Hello ");
+    System.out.println("world!!!!");
+    System.out.printf("%s","You can use printf, too!!!");
+  }
+}
