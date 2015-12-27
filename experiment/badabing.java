bb072eddd76ba4f71af848b9ff689b69d5df8326
nickmooregot33 <nickmooregot33@gmail.com>
2015-12-27 12:25:51 -0600

    added type checking exception handling to badabing

--- experiment/badabing.java
+++ experiment/badabing.java
@@ -6,40 +6,78 @@
  * 	function calls, at the moment.                 *
  *******************************************************/
 
+import java.util.Scanner; 	//import the scanner
 
-import java.util.Scanner;
+//still need to clarify 'static' vs 'non-static' function behaviors and implementation specifics
 public class badabing {
 	public static void main(String args[]){
 	/*	System.out.printf("%s",args[0]);*/	// apparently the program name is not passed to 'main' in java
+
 	//print out any argument strings passed to 'main'
 		if (args.length == 0) 
 			System.out.printf("%s","You didn't give an argument.\n");
-		else if (args.length == 1)
-			System.out.printf("%s.\n",args[0]);
 		else{ 
 			for (String s : args)
 				System.out.printf("%s ",s);
 			System.out.printf("\n");
 		}
-
-	//still need to clarify 'static' vs 'non-static' behaviors
-	//	badabing tester = new badabing(); //haven't implemented a constructor at this point in time
-		
+	//test the getString method		
 		System.out.printf("You gave us:\t\'%s\'\n",getString());
+	//test the getInt method		
+		System.out.printf("You gave us:\t\'%s\'\n",getInt());
+	//test the getDouble method		
+		System.out.printf("You gave us:\t\'%s\'\n",getDouble());
 	};
+	
+/*******************************************************
+ * getString()                                         *
+ * 	gets a line from standard input                *
+ * 	inputs - none                                  *
+ * 	returns - newline terminated String            *
+ *******************************************************/
 	public static String getString(){
-
 		Scanner getter = new Scanner(System.in);
 		System.out.printf("Give us a new string.\n");
-		String returnval = new String(getter.nextLine());
-		return returnval;
+		return getter.nextLine();
 	};
 
+/*******************************************************
+ * getInt()                                            *
+ * 	gets an Int from standard input                *
+ * 	inputs - none                                  *
+ * 	returns - the int obtained from standard in    *
+ *******************************************************/
 	public static int getInt(){
-		return 0;
+		Scanner getter = new Scanner(System.in);
+		while(true){
+			System.out.printf("Give us a new Int.\n");
+			try{
+				return getter.nextInt();
+			}
+			catch(java.util.InputMismatchException not_an_int){
+				System.out.println("You gave us something else.");			
+				getter.next();
+			}
+		}
 	};
 
+/*******************************************************
+ * getDouble()                                         *
+ * 	gets a Double from standard input              *
+ * 	inputs - none                                  *
+ * 	returns - the Double obtained from standard in *
+ *******************************************************/
 	public static double getDouble(){
-		return 0.0;
+		Scanner getter = new Scanner(System.in);
+		while(true){
+			System.out.printf("Give us a new floating point number.\n");
+			try{
+				return getter.nextDouble();
+			}
+			catch(java.util.InputMismatchException not_a_double){
+				System.out.println("You gave us something else.");			
+				getter.next();
+			}
+		}
 	};
 }
