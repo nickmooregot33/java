8cbc0b9f68d60caec778a9037621d3d462165c40
nickmooregot33 <nickmooregot33@gmail.com>
2015-12-27 18:06:05 -0600

    renamed badabing to scannerWrapper.  Will probably rename it in the future, as it won't implement a wrapped function for every method in java.util.Scanner, because hasNextObject doesn't really need anything.

--- experiment/scannerWrapper.java
+++ experiment/scannerWrapper.java
@@ -9,7 +9,7 @@
 import java.util.Scanner; 	//import the scanner
 
 //still need to clarify 'static' vs 'non-static' function behaviors and implementation specifics
-public class badabing {
+public class scannerWrapper{
 	public static void main(String args[]){
 	/*	System.out.printf("%s",args[0]);*/	// apparently the program name is not passed to 'main' in java
 
@@ -21,12 +21,21 @@ public class badabing {
 				System.out.printf("%s ",s);
 			System.out.printf("\n");
 		}
-	//test the getString method		
-		System.out.printf("You gave us:\t\'%s\'\n",getString());
+	String messages[] = {	"Give us a",		//beginning of message
+	       			"n Int.\n",		//datatype  
+				" String.\n",		//datatype 
+				" Long.\n",		//datatype 
+				" Double.\n",		//datatype 
+				"You gave us something else.\n"	//message_two
+			};
+	//test the getLine method		
+		System.out.printf("You gave us:\t\'%s\'\n",getLine(messages[0]+messages[2]));
 	//test the getInt method		
-		System.out.printf("You gave us:\t\'%s\'\n",getInt());
+		System.out.printf("You gave us:\t\'%s\'\n",getInt(messages[0]+messages[1], messages[5]));
+	//test the getLong method		
+		System.out.printf("You gave us:\t\'%s\'\n",getLong(messages[0]+messages[3], messages[5]));
 	//test the getDouble method		
-		System.out.printf("You gave us:\t\'%s\'\n",getDouble());
+		System.out.printf("You gave us:\t\'%s\'\n",getDouble(messages[0]+messages[4], messages[5]));
 	};
 	
 /*******************************************************
@@ -35,9 +44,9 @@ public class badabing {
  * 	inputs - none                                  *
  * 	returns - newline terminated String            *
  *******************************************************/
-	public static String getString(){
+	public static String getLine(String message){
 		Scanner getter = new Scanner(System.in);
-		System.out.printf("Give us a new string.\n");
+		System.out.printf("%s",message);
 		return getter.nextLine();
 	};
 
@@ -47,19 +56,40 @@ public class badabing {
  * 	inputs - none                                  *
  * 	returns - the int obtained from standard in    *
  *******************************************************/
-	public static int getInt(){
+	public static int getInt(String message_one, String message_two){
 		Scanner getter = new Scanner(System.in);
 		while(true){
-			System.out.printf("Give us a new Int.\n");
+			System.out.printf("%s",message_one);	//"Give us a new Int."
 			try{
 				return getter.nextInt();
 			}
 			catch(java.util.InputMismatchException not_an_int){
-				System.out.println("You gave us something else.");			
+				System.out.println(message_two);	//"You gave us something else."			
 				getter.next();
 			}
 		}
 	};
+
+/*******************************************************
+ * getLong()                                           *
+ * 	gets a long from standard input                *
+ * 	inputs - none                                  *
+ * 	returns - the long obtained from standard in   *
+ *******************************************************/
+	public static long getLong(String message_one, String message_two){
+		Scanner getter = new Scanner(System.in);
+		while(true){
+			System.out.printf("%s",message_one);	//"Give us a new Int."
+			try{
+				return getter.nextLong();
+			}
+			catch(java.util.InputMismatchException not_a_long){
+				System.out.println(message_two);	//"You gave us something else."			
+				getter.next();
+			}
+		}
+	};
+
 
 /*******************************************************
  * getDouble()                                         *
@@ -67,15 +97,15 @@ public class badabing {
  * 	inputs - none                                  *
  * 	returns - the Double obtained from standard in *
  *******************************************************/
-	public static double getDouble(){
+	public static double getDouble(String message_one, String message_two){
 		Scanner getter = new Scanner(System.in);
 		while(true){
-			System.out.printf("Give us a new floating point number.\n");
+			System.out.printf("%s",message_one);	//"Give us a new floating point numbern"
 			try{
 				return getter.nextDouble();
 			}
 			catch(java.util.InputMismatchException not_a_double){
-				System.out.println("You gave us something else.");			
+				System.out.println(message_two); 	//"You gave us something else."			
 				getter.next();
 			}
 		}
