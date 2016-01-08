cdfd82b27666c3720e0795dbf67b8e392a0fb217
nickmooregot33 <nickmooregot33@gmail.com>
2016-01-08 10:42:57 -0600

    added Addition.java, a small class to play with exceptions more and some introductory GUI

--- graphics/Addition.java
+++ graphics/Addition.java
@@ -1,23 +1,56 @@
 import javax.swing.JOptionPane;
+import java.lang.Exception;
 
 public class Addition{
 	public static void main( String args[] ){
-		boolean always = true;
-		while(always){
-			String firstNumber = JOptionPane.showInputDialog("Enter the dragon");
-			String secondNumber = JOptionPane.showInputDialog("Enter the fist");
+		Addition obj = new Addition();
 
+		boolean test = true;
+		int number1 = 0;
+		int number2 = 0;
 
+		//get input using message boxes
+		try{
+			number1 = obj.getData();
+			number2 = obj.getData();
+		}
+		catch(Exception e){
+			test = false;
+		}
+		
+
+		//use the input; do the sum
+		if (test){
+			int sum = number1 + number2;
+
+			//give the output
+			JOptionPane.showMessageDialog( 
+				null, 
+				"The sum is " + sum, 
+				"Sum of Two Integers",
+			       	JOptionPane.PLAIN_MESSAGE
+			);
+		}
+	}
+//###########################################################################################
+	private int getData() throws Exception{
+		//repeat until user cancels or inputs valid data
+		while(true){
 			try{
-				int number1 = Integer.parseInt(firstNumber);
-				int number2 = Integer.parseInt(secondNumber);
-				int sum = number1 + number2;
-				JOptionPane.showMessageDialog( null, "The sum is " + sum, "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE);
-				break;
+				String numString;
+				int number;
+				if ((numString = JOptionPane.showInputDialog("Enter the dragon")) == null) throw new Exception();
+				number = Integer.parseInt(numString);
+				return number;
 			}
-			catch(java.lang.NumberFormatException dumbException){
-				JOptionPane.showMessageDialog( null, "You didn't give me two integers.  Perhaps I shouldn't use movie names as the prompt.", "Exception thrown.  Not integers", JOptionPane.PLAIN_MESSAGE);
-			};
+			catch(java.lang.NumberFormatException wrong_input){
+				JOptionPane.showMessageDialog( 	
+					null, 
+					"What I really want is for you to enter an integer.", 
+					"Exception thrown.  Not an integer", 
+					JOptionPane.WARNING_MESSAGE
+				     	);
+			}
 		}
 	}
 }
